9813a367bfbee0f983d915ce7e5a1d8d0794a158
unknown <tarnof@hotmail.com>
2022-10-13 15:36:22 +0200

    cards affichées

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent, useState,useEffect} from 'react';
 import Pokemon from './models/pokemon';
 import POKEMONS from './models/mock-pokemons';
 
   
 const App: FunctionComponent = () => {
- const[pokemons] = useState<Pokemon[]>(POKEMONS);
+ const[pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+ useEffect(()=>{
+    setPokemons(POKEMONS)
+ },[])
+ 
     
  return (
     <div>
-        <h1>Pokédex</h1>
-        <p>Il y a {pokemons.length} pokemons dans votre application.</p>
+        <h1 className="center">Pokédex</h1>
+        <p className="center">Il y a {pokemons.length} pokemons dans le Pokédex.</p>
+        <div className="container">
+            <div className="row">
+            {pokemons.map(({name, id , picture, created}) => (
+               <div className="col s6 m4" key={id}>
+               <div className="card horizontal">
+                  <div className="card-image">
+                  <img src={picture} alt="name"/>
+                  </div>
+                  <div className="card-stacked">
+                  <div className="card-content">
+                     <p>{name}</p>
+                     <p><small>{created.toString()}</small></p>
+                  </div>
+                  </div>
+               </div>
+               </div>                       
+        ))}
+            </div>
+        </div>    
   </div>
  )
 }
